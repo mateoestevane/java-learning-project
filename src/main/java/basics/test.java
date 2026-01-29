c7cf967db68ec1ec674565eee58daceb76930f3c
Mateo Gomez <mateoestevanegt@gmail.com>
2026-01-28 23:59:44 -0600

    ejercicios 3 y 4 if else

---
+++ src/main/java/basics/test.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("WE NEED TO\n");
+        System.out.println("LEARN JAVA\n");
+        System.out.println("AS QUICKLY AS POSSIBLE\n");
+
+    }
+}
